903d1af20ab44f660474f12d4f7b1d98375d2fc7
Taran66 <taranpreet1911@gmail.com>
2024-04-17 18:02:07 +0530

    made changes in Contact section

--- src/section/Contact.jsx
+++ src/section/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaTelegram } from "react-icons/fa";
+import { FaInstagram, FaLinkedinIn, FaPhoneAlt, FaTelegram} from 'react-icons/fa';
 
 const Contact = () => {
   return (
@@ -11,22 +11,37 @@ const Contact = () => {
             <a href="#Contact">
               <h2 className="text-slate-800 text-8xl z-10 hover:text-amber-400 hover:blur-sm transition-all duration-500 ease-in-out hover:translate-y-5">Contact</h2>
             </a>
-          <h3 className="text-center text-amber-400 z-20 text-4xl absolute top-0">Contact</h3>
+            <h3 className="text-center text-amber-400 z-20 text-4xl absolute top-0">Contact</h3>
+          </div>
+          <div className='flex gap-40'>
+          <div className='text-white flex flex-col gap-20 '>
+              <h2 className='text-7xl'>Contact Me</h2>
+              <FaTelegram className='text-4xl'/>
+              <FaPhoneAlt className='text-4xl'/>
+              <div className='flex text-4xl gap-20'>
+                <a href="https://www.instagram.com/_taran._.singh_/">
+                  <FaInstagram className='hover:-translate-y-2 hover:text-amber-400 duration-300 ease-in-out' />
+                </a>
+                <a href="https://www.linkedin.com/in/taran-saini/">
+                  <FaLinkedinIn className='hover:-translate-y-2 hover:text-amber-400 duration-300 ease-in-out'/>
+                </a>
+              </div>
+              <button className='text-3xl bg-amber-400 rounded-md w-8/12 p-3 hover:text-slate-950'>Download CV</button>
+          </div>
+          <div className='flex w-96'>
+            <form className='flex flex-col gap-10 w-full'>
+              <input type="text" className='rounded-lg h-12'/>
+              <input type="text" className='rounded-lg h-12'/>
+              <textarea className='h-48 rounded-lg'/>
+            </form>
           </div>
         </div>
+        </div>
+        
         
       </div>
     </div>
-        {/* <div>
-            <h2>Contact Me</h2>
-            <div>
-            <FaTelegram />
-
-            </div>
-        </div>
-        <div>
-
-        </div> */}
+        
     </div>
   )
 }
